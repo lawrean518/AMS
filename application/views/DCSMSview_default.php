423b7b38111241c96dfb2ab92f68bd0519b8efbb
lawrean518 <lawrean_angeley518@yahoo.com>
2015-02-24 02:36:00 +0800

    commit message

--- application/views/DCSMSview_default.php
+++ application/views/DCSMSview_default.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
 
-	<!--<link rel="stylesheet" href="< // ?php echo base_url("assets/css/styles.css"); ?>" />-->
-	<!--link rel="shortcut icon" href="img/dcs_logo.ico"-->
-	<title>AMS</title>
+  <!--<link rel="stylesheet" href="< // ?php echo base_url("assets/css/styles.css"); ?>" />-->
+  <!--link rel="shortcut icon" href="img/dcs_logo.ico"-->
+  <title>AMS</title>
 </head>
 <body>
 <body background = "img/congruent_pentagon.png">
@@ -23,28 +23,35 @@
           <br>
           <form class="col-lg-12">
             <div class="input-group" style="width:400px;text-align:center;margin:0 auto;">
-            <input class="form-control input-lg" title="Search" placeholder="Search" type="text">
+            <input class="form-control input-lg" title="" placeholder="Search" type="text">
               <span class="input-group-btn">
               <button type="button" class="btn btn-lg btn-success dropdown-toggle" data-toggle="dropdown">Student Number<span class = "caret"></button>
-   				<ul class="dropdown-menu" role="menu">
-		    	<li><a href="#">Student Number</a></li>
-		    	<li><a href="#">Last Name</a></li>
-		    	<li><a href="#"></a></li>
-   				</ul></span>
-            	
+          <ul class="dropdown-menu" role="menu">
+          <li><a href="#">Student Number</a></li>
+          <li><a href="#">Last Name</a></li>
+          <li><a href="#"></a></li>
+          </ul></span>
+              
             </div>
-            <br><button class="btn btn-lg btn-primary" type="button">SEARCH</button><p>        </p><button class="btn btn-lg btn-primary" type="button">SHOW ALL</button>
+            <br><button class="btn btn-lg btn-primary" id="search" type="button">SEARCH</button><p>        </p><button class="btn btn-lg btn-primary" type="button">SHOW ALL</button>
           </form>
         </div>
         
       </div> <!-- /row -->
   
-  	<br><br><br><br><br>
+    <br><br><br><br><br>
 
 </div> <!-- /container full -->
 
 
 <script type="text/javascript" src="<?php echo base_url("assets/js/jquery-1.11.2.min.js"); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+<script> 
+  $(document).ready(function(){
+    $("#search").click(function(){
+        $("#search").hide();
+    });
+  });
+</script>
 </body>
 </html>
